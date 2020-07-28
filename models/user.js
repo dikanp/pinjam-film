fb99c76158c093fe5842491903698f2fddab00d8
dikanp <andika.dikanp@gmail.com>
2020-07-28 18:43:42 +0700

    add lodash and password validation with regex

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
-
-
+const passwordComplexity = require("joi-password-complexity");
+  
 const User = mongoose.model('User', new mongoose.Schema({
     name: {
         type: String,
@@ -21,19 +21,32 @@ const User = mongoose.model('User', new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8,
-        maxlength: 1024
+        maxlength: 1024    
     }
-    
+
+
 }));
 
 function validateUser(user) {
+    const complexityOptions = {
+        min: 5,
+        max: 250,
+        lowerCase: 1,
+        upperCase: 1,
+        numeric: 1,
+        symbol: 1,
+        requirementCount: 2,
+      };
+
     const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(8).max(1024).required()
+        // password: passwordComplexity(complexityOptions)
+        password: Joi.string().regex(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])")).min(5).max(255).required(),
     });
-    return schema.validate(user);
-    
+    // return schema.validate(user);
+    const {error} = schema.validate(user);
+    return error;
 }
 
 exports.User = User;
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,39 @@
 const {User, validate} = require('../models/user')
 const mongoose = require('mongoose');
 const express = require('express');
+const _ = require('lodash');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    const user = await User.find().sort('name');
+    res.send(user);
+});
+
 router.post('/', async (req, res) => {
-    // const user = await User.find().sort('name');
-    // res.send(user);
+    
 
-    const {error} = validate(req.body);
+    const error = validate(req.body);
 
-    if (error) {
-        return res.status(400).send(error.details[0].message)
-    };
+    // if (error) {
+    //     return res.status(400).send(error.details[0].message)
+    // };
+    console.log(error)
+    if (error) return res.status(400).send(`A new user could not be created. ${error.details[0].message}`);
 
     let user = await User.findOne({ email: req.body.email});
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User ({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User (_.pick(req.body, ['name', 'email', 'password']))
+    // user = new User(
+    //     {
+    //         name: req.body.name,
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     }
+    // );
 
     user = await user.save();
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
