4592c8c2a90eba9bc9d2e33b88a7c5a9515329c4
dikanp <andika.dikanp@gmail.com>
2020-07-29 11:06:51 +0700

    add hash salt

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
-const passwordComplexity = require("joi-password-complexity");
-  
+// const passwordComplexity = require("joi-password-complexity");
+// var passwordValidator = require('password-validator');
+
 const User = mongoose.model('User', new mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 50,
+        maxlength: [50, 'name length too long'],
         unique: [true, 'name must be unique']
     },
     email: {
@@ -21,28 +22,26 @@ const User = mongoose.model('User', new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8,
-        maxlength: 1024    
+        maxlength: 1024,    
+        select: false
     }
 
 
 }));
 
 function validateUser(user) {
-    const complexityOptions = {
-        min: 5,
-        max: 250,
-        lowerCase: 1,
-        upperCase: 1,
-        numeric: 1,
-        symbol: 1,
-        requirementCount: 2,
-      };
-
     const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         // password: passwordComplexity(complexityOptions)
-        password: Joi.string().regex(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])")).min(5).max(255).required(),
+        password: Joi.string().regex(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])")).min(8).max(1024).required(),
+        // password: new passwordValidator()
+        // .is().min(8)                             
+        // .is().max(100)                                  // Maximum length 100
+        // .has().uppercase()                              // Must have uppercase letters
+        // .has().lowercase()                              // Must have lowercase letters
+        // .has().digits()                                 // Must have digits
+        // .has().not().spaces() 
     });
     // return schema.validate(user);
     const {error} = schema.validate(user);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 const {User, validate} = require('../models/user')
 const mongoose = require('mongoose');
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     const user = await User.find().sort('name');
     res.send(user);
+    // res.send(users.map((user) => {
+    //     return _.pick(user, ['_id', 'name', 'email']);
+    // }));
+    // res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 router.post('/', async (req, res) => {
@@ -18,12 +23,16 @@ router.post('/', async (req, res) => {
     //     return res.status(400).send(error.details[0].message)
     // };
     console.log(error)
+    if (error.details[0].message.includes('"name" length must be')) return res.status(400).send(`Nama kurang panjang `);
     if (error) return res.status(400).send(`A new user could not be created. ${error.details[0].message}`);
 
     let user = await User.findOne({ email: req.body.email});
     if (user) return res.status(400).send('User already registered.');
 
     user = new User (_.pick(req.body, ['name', 'email', 'password']))
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    
     // user = new User(
     //     {
     //         name: req.body.name,
